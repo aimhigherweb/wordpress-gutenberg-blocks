09c54bc6aaec770b25b3fbada6b8bf0ddf7a5afd
Amy Kapernick <amy@aimhigherweb.design>
2020-04-22 10:49:20 +0800

    added banner block

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import colourContainer from './src/blocks/colour_container'
 import teamProfile from './src/blocks/team_profile'
 import Faq from './src/blocks/faq'
+import Banner from './src/blocks/banner'
 
 colourContainer()
 teamProfile()
 Faq()
+Banner()
---
+++ src/blocks/banner/index.js
@@ -0,0 +1,93 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { MediaUpload } from '@wordpress/block-editor';
+import { Button, TextControl } from '@wordpress/components';
+
+
+const Banner = () => {
+	registerBlockType('aimhigher/banner', {
+		title: 'Banner',
+		icon: 'align-center',
+		category: 'common',
+		attributes: {
+			text: {
+				type: 'string',
+				selector: '.banner blockquote',
+				source: 'text'
+			},
+			cta_text: {
+				type: 'string',
+				selector: '.cta',
+				source: 'text'
+			},
+			cta_url: {
+				type: 'string',
+				selector: '.cta',
+				source: 'attribute',
+				attribute: 'href'
+			},
+			image: {
+				type: 'string',
+			},
+		},
+		edit(props) {
+			let banner = props.attributes.image,
+			styles = {}
+			
+			if(banner) {
+				styles = {'--bannerImage': `url(${banner})` }
+			}
+			
+			return (
+				<div className="banner" id="block-editable-box" style={styles}>
+					<label>Banner Text</label>
+					<TextControl
+						value={props.attributes.text}
+						onChange={(text) => {props.setAttributes({text: text})}}
+					/>
+					<label>Image</label>
+					<MediaUpload
+						onSelect={(newBanner) => {props.setAttributes({image: newBanner.url})}}
+						allowedTypes="image"
+						value={ banner == undefined ? 'Select Image' : banner }
+						render={ ( { open } ) => (
+							<Button onClick={ open }>
+								{ banner == undefined ? 'Upload Image' : <img src={ banner } /> }
+							</Button>
+						) }
+					/>
+					<label>CTA Text</label>
+					<TextControl
+						value={props.attributes.cta_text}
+						onChange={(cta_text) => {props.setAttributes({cta_text: cta_text})}}
+					/>
+					<label>CTA Url</label>
+					<TextControl
+						value={props.attributes.cta_url}
+						onChange={(cta_url) => {props.setAttributes({cta_url: cta_url})}}
+					/>
+				</div>
+			);
+		},
+	
+		save(props) {
+			let banner = props.attributes.image,
+			styles = {}
+			
+			if(banner) {
+				styles = {'--bannerImage': `url(${banner})` }
+			}
+
+			console.log(props.attributes)
+
+			return (
+				<div className="banner" style={styles}>
+					{props.attributes.text && <blockquote>{props.attributes.text}</blockquote>}
+					{props.attributes.cta_text && <a href={props.attributes.cta_url} className="btn cta">{props.attributes.cta_text}</a>}
+				</div>
+			);
+
+		},
+	});
+}
+
+export default Banner
